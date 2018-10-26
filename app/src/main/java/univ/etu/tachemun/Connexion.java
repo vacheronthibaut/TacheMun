51faa7487705f4025f6058003a10d15902b7e68f
Vacheron thibaut <blacpiece@gmail.com>
2018-10-26 10:39:43 +0200

    + private Objet pour connexion

--- app/src/main/java/univ/etu/tachemun/Connexion.java
+++ app/src/main/java/univ/etu/tachemun/Connexion.java
@@ -2,8 +2,13 @@ package univ.etu.tachemun;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Connexion extends AppCompatActivity {
+
+    private EditText pseudo;
+    private EditText mdp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
